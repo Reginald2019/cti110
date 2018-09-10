0da46321db5439751eb88f731d637a7ac81ecaff
Reginald2019 <43155644+Reginald2019@users.noreply.github.com>
2018-09-10 19:24:43 -0400

    Add files via upload
    First Submission.

---
+++ P2T1_SalesPrediction_ReginaldRichardson.py
@@ -0,0 +1,14 @@
+# Calculating profit of total sales
+# Date
+# CTI-110 P2T1 - Sales Prediction
+# Reginald Richardson
+
+# Get the projected total sales.
+total_sales = float(input('Enter projected sales: '))
+
+# Calculate profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+
+# Display profit.
+print('The profit is $', format(profit, ',.2f'))
